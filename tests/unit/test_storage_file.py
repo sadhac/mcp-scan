2034d9ce85e33ed1c50b48d6977f7506e457b9dd
Marc Fischer <mail@marcfischer.at>
2025-04-24 09:37:57 +0200

    fixed test

--- tests/unit/test_storage_file.py
+++ tests/unit/test_storage_file.py
@@ -6,22 +6,24 @@ def test_whitelist():
     with TemporaryDirectory() as tempdir:
         path = os.path.join(tempdir, "storage.json")
         storage_file = StorageFile(path)
-        storage_file.add_to_whitelist("test", "test")
-        storage_file.add_to_whitelist("test", "test2")
-        storage_file.add_to_whitelist("asdf", "test2")
+        storage_file.add_to_whitelist('tool', "test", "test")
+        storage_file.add_to_whitelist('tool', "test", "test2")
+        storage_file.add_to_whitelist('tool', "asdf", "test2")
         assert len(storage_file.whitelist) == 2
         assert storage_file.whitelist == {
-            "test": "test2",
-            "asdf": "test2",
+            "tool.test": "test2",
+            "tool.asdf": "test2",
         }
         storage_file.save()
         
+        # test reload
+        storage_file = StorageFile(path)
+        assert len(storage_file.whitelist) == 2
+       
+        # test reset 
         storage_file.reset_whitelist()
         assert len(storage_file.whitelist) == 0
         
-        storage_file = StorageFile(path)
-        assert len(storage_file.whitelist) == 2
-        
-        
+       
         
         
